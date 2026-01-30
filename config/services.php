a3edcaa6b535f7fb8991db9b109144ea13edf1e7
rkamgochetchom@gk-software.com <rkamgochetchom@gk-software.com>
2026-01-30 17:15:24 +0100

    enable google authentication

--- config/services.php
+++ config/services.php
@@ -41,5 +41,10 @@ return [
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'redirect' => env('GITHUB_CLIENT_CALLBACK_URL'),
     ],
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CLIENT_CALLBACK_URL'),
+    ],
 
 ];
